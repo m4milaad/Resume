68fb10dd79cd26e6e23ab2e4712e00d70cdaed30
m4milaad <mb4milad.bhattt@gmail.com>
2025-05-29 19:07:18 +0530

    changed light follow duration

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -656,7 +656,7 @@ const MouseGlow = document.getElementById("MouseGlow");
 // Make mouse glow follow mouse
 
 window.addEventListener("mousemove", (event) => {
-    MouseGlow.animate({left: `${event.clientX}px`, top: `${event.clientY}px`}, {duration: 2000, fill: "forwards"});
+    MouseGlow.animate({left: `${event.clientX}px`, top: `${event.clientY}px`}, {duration: 3500, fill: "forwards"});
 });
 
 // Scrollspy logic // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
